22c338a6b2494c5a595177f3168e16d59306beab
Nan Sheng <95516781+sheng-n@users.noreply.github.com>
2024-09-02 14:03:33 +0800

    Update training.py

--- code/training.py
+++ code/training.py
@@ -57,7 +57,7 @@ def predicting(model, device, loader):
 
     print(f"Accuracy: {accuracy:.4f}, Precision: {precision:.4f}, Recall: {recall:.4f}, F1 Score: {f1:.4f}")
 
-    # 模型参数评估
+    # Evaluation metrics
     roc_auc = roc_auc_score(total_labels, total_probs)
     precision_vals, recall_vals, _ = precision_recall_curve(total_labels, total_probs)
     sorted_indices = np.argsort(recall_vals)
@@ -100,7 +100,7 @@ processed_data_file_test = 'data/processed/' + 'test1.pt'
 if ((not os.path.isfile(processed_data_file_train)) or (not os.path.isfile(processed_data_file_test))):
     print('please run process_data.py to prepare data in pytorch format!')
 else:
-    #模型训练
+    #train
     device = torch.device(cuda_name if torch.cuda.is_available() else "cpu")
     accuracies = []
     precisions = []
@@ -129,7 +129,7 @@ else:
         roc_aucs.append(roc_auc)
         pr_aucs.append(pr_auc)
 
-    # 模型评估
+    # average
     avg_accuracy = np.mean(accuracies)
     avg_precision = np.mean(precisions)
     avg_recall = np.mean(recalls)
